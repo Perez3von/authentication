107bf4a24e27f7abab37f829a2cc74ebf3c31629
Evon Perez <perez.evon@gmail.com>
2021-10-07 20:39:56 -0400

    passing login test;

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+//const UserService = require('../lib/services/UserService');
 const User = require('../lib/models/User.js');
 
 describe('authentication routes', () => {
@@ -15,23 +16,46 @@ describe('authentication routes', () => {
 
 
     const resp = await request(app).post('/api/auth/signup').send({ email:'tom@jerry.com', password:'ilovethatshow' });
+    console.log(resp.body);
     expect(resp.body).toEqual({ id:1, email:'tom@jerry.com' });
 
   });
   //---------------------------------------------------------//
   it('gets user from database that exists', async () => {
     const user = 'tom@jerry.com';
-
+    let bool = false;
     const resp = await User.getUser(user); //;
-    expect(resp).toEqual({ id:1, email:'tom@jerry.com' });
+    if(resp) bool = true; 
+    expect(bool).toEqual(true);
+
+  });
+  //---------------------------------------------------------//
+  it('tries to get user from database that doesnt exist', async () => {
+    const user = 'idontexist@email.com';
+    let bool = false;
+    const resp = await User.getUser(user); //;
+    if(resp) bool = true; 
+    expect(bool).toEqual(false);
 
   });
 
-  it('tries to get user from database that doesnt exist', async () => {
-    const user = 'idontexist@email.com';
+  //-----------------------------------------------//
+  it('/signup returns error 404', async () => {
 
-    const resp = await User.getUser(user);//;
-    expect(resp).toEqual(null);
+
+    const resp = await request(app).post('/api/auth/signup').send({ email:'tom@jerry.com', password:'ilovethatshow' });
+   
+    expect(resp.status).toEqual(401);
+
+  });
+
+  //---------------------------------------------------------//
+  it('/login returns user id info without password', async () => {
+
+
+    const resp = await request(app).post('/api/auth/login').send({ email:'tom@jerry.com', password:'ilovethatshow' });
+    const user = await User.getUser('tom@jerry.com');
+    expect(resp.body.id).toEqual(user.id);
 
   });
 
